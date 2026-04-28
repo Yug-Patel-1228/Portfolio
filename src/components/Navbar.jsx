8b8fd4a362924e2f9759c37c9623f56dfac08763
Yug-Patel-1228 <yugcpatel2812@gmail.com>
2026-04-28 16:09:42 +0530

    Removed Resume and fixed footer.
    Removed Resume and fixed footer.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { TbDownload } from "react-icons/tb";
 import { HiOutlineMenu, HiX } from "react-icons/hi";
 import logo from "../assets/logo.png"; 
 
@@ -109,17 +108,6 @@ export default function Navbar() {
                   </button>
                 </motion.li>
               ))}
-              <motion.a
-                href=""
-                className="relative inline-block px-4 py-2 font-semibold group"
-                whileHover={{ scale: 1.1 }}
-              >
-                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span className="relative text-black group-hover:text-white flex items-center gap-x-3">
-                  Resume <TbDownload size={16} />
-                </span>
-              </motion.a>
             </ul>
           </motion.div>
         )}
